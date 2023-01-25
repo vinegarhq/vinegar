664a7bd028336ecfd0910ebbd0e0873e6853a71e
lunarlattice <lunar.lattice0@gmail.com>
2023-01-25 15:20:22 -0800

    initial struct

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/nocrusts/vinegar
+
+go 1.19
+
+require github.com/adrg/xdg v0.4.0
+
+require golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
--- src/main.go
+++ src/main.go
@@ -1,14 +1,17 @@
 // User should not have to interact with this program at any time.
 
+package main
+
 import (
-	"os"
+//	"os"
 	"fmt"
-	"log"
+//	"log"
+	"github.com/adrg/xdg"
 )
 
-func build_prefix() {
-	
-}
+//func build_prefix() {
+//}
+
 func main() {
-	
+	fmt.Println(xdg.DataHome)
 }

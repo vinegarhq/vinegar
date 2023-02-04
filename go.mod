6b1d5df6777c6f628d1aaac484135c3ef2ac6eeb
wael <sewn@disroot.org>
2023-02-04 07:28:50 +0300

    re-structure project

--- go.mod
+++ go.mod
@@ -5,36 +5,27 @@ go 1.19
 require github.com/hashicorp/go-getter v1.6.2
 
 require (
-	cloud.google.com/go v0.109.0 // indirect
-	cloud.google.com/go/compute v1.18.0 // indirect
-	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/iam v0.10.0 // indirect
-	cloud.google.com/go/storage v1.29.0 // indirect
-	github.com/aws/aws-sdk-go v1.44.193 // indirect
+	cloud.google.com/go v0.45.1 // indirect
+	github.com/aws/aws-sdk-go v1.15.78 // indirect
 	github.com/bgentry/go-netrc v0.0.0-20140422174119-9fd32a8b3d3d // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.2.1 // indirect
-	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-safetemp v1.0.0 // indirect
-	github.com/hashicorp/go-version v1.6.0 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/klauspost/compress v1.15.15 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
-	github.com/ulikunitz/xz v0.5.11 // indirect
-	go.opencensus.io v0.24.0 // indirect
-	golang.org/x/net v0.5.0 // indirect
-	golang.org/x/oauth2 v0.4.0 // indirect
-	golang.org/x/sys v0.4.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
-	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
-	google.golang.org/api v0.109.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20230202175211-008b39050e57 // indirect
-	google.golang.org/grpc v1.52.3 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	github.com/hashicorp/go-version v1.1.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/jmespath/go-jmespath v0.0.0-20160202185014-0b12d6b521d8 // indirect
+	github.com/klauspost/compress v1.11.2 // indirect
+	github.com/mitchellh/go-homedir v1.0.0 // indirect
+	github.com/mitchellh/go-testing-interface v1.0.0 // indirect
+	github.com/ulikunitz/xz v0.5.8 // indirect
+	go.opencensus.io v0.22.0 // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
+	golang.org/x/sys v0.0.0-20220517195934-5e4e11fc645e // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/api v0.9.0 // indirect
+	google.golang.org/appengine v1.6.1 // indirect
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55 // indirect
+	google.golang.org/grpc v1.21.1 // indirect
 )
---
+++ util.go
@@ -0,0 +1,141 @@
+// Copyright vinegar-development 2023
+
+package vinegar
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	"github.com/hashicorp/go-getter"
+)
+
+type Dirs struct {
+	Cache string
+	Data  string
+	Log   string
+	Pfx   string
+	Exe   string
+}
+
+func Errc(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func DeleteDir(dir ...string) {
+	for _, d := range dir {
+		log.Println("Deleting directory:", d)
+		Errc(os.RemoveAll(d))
+	}
+}
+
+func DirsCheck(dir ...string) {
+	for _, d := range dir {
+		if _, err := os.Stat(d); os.IsNotExist(err) {
+			log.Println("Creating directory:", d)
+		} else { 
+			continue
+		}
+		Errc(os.MkdirAll(d, 0755))
+	}
+}
+
+func InitDirs() *Dirs {
+	dirs := new(Dirs)
+	home := os.Getenv("HOME")
+
+	if home == "" {
+		log.Fatal("Failed to get $HOME variable")
+	}
+
+	dirs.Cache = (filepath.Join(home, ".cache", "/vinegar"))
+	dirs.Data  = (filepath.Join(home, ".local", "share", "/vinegar"))
+	dirs.Log   = (filepath.Join(dirs.Cache, "/logs"))
+	dirs.Pfx   = (filepath.Join(dirs.Data, "/pfx"))
+	dirs.Exe   = (filepath.Join(dirs.Cache, "/exe"))
+
+	return dirs
+}
+
+func InitEnv(dirs *Dirs) {
+	os.Setenv("WINEPREFIX", dirs.Pfx)
+	os.Setenv("WINEARCH", "win64")
+	// Removal of most unnecessary Wine facilities
+	os.Setenv("WINEDEBUG", "fixme-all,-wininet,-ntlm,-winediag,-kerberos,+relay")
+	os.Setenv("WINEDLLOVERRIDES", "dxdiagn=d;winemenubuilder.exe=d")
+	
+	os.Setenv("DXVK_LOG_LEVEL", "warn")
+	os.Setenv("DXVK_LOG_PATH", "none")
+	os.Setenv("DXVK_STATE_CACHE_PATH", filepath.Join(dirs.Cache, "dxvk"))
+	
+	os.Setenv("MESA_GL_VERSION_OVERRIDE", "4.4")
+	// Use the dedicated gpu if available, untested
+	os.Setenv("DRI_PRIME", "1")
+	os.Setenv("__NV_PRIME_RENDER_OFFLOAD", "1")
+	os.Setenv("__VK_LAYER_NV_optimus", "NVIDIA_only")
+	os.Setenv("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+}
+
+func Exec(dirs *Dirs, prog string, args ...string) {
+	log.Println(args)
+	cmd := exec.Command(prog, args...)
+	timeFmt := time.Now().Format(time.RFC3339)
+
+	stdoutFile, err := os.Create(filepath.Join(dirs.Log, timeFmt + "-stdout.log"))
+	Errc(err)
+	log.Println("Forwarding stdout to", stdoutFile.Name())
+	defer stdoutFile.Close()
+
+	stderrFile, err := os.Create(filepath.Join(dirs.Log, timeFmt + "-stderr.log"))
+	Errc(err)
+	log.Println("Forwarding stderr to", stderrFile.Name())
+	defer stderrFile.Close()
+	
+	cmd.Dir = dirs.Cache
+	cmd.Stdout = stdoutFile
+	cmd.Stderr = stderrFile
+
+	Errc(cmd.Run())
+
+	logFile, err := stderrFile.Stat()
+	Errc(err)
+	if logFile.Size() == 0 {
+		log.Println("Empty stderr log file detected, deleting")
+		Errc(os.RemoveAll(stderrFile.Name()))
+	}
+
+	logFile, err = stdoutFile.Stat()
+	Errc(err)
+	if logFile.Size() == 0 {
+		log.Println("Empty stdout file detected, deleted")
+		Errc(os.RemoveAll(stdoutFile.Name()))
+	}
+}
+
+func InitExec(dirs *Dirs, path string, url string, what string) (string) {
+	path = filepath.Join(dirs.Exe, path)
+
+	_, err := os.Stat(path)
+
+	if os.IsNotExist(err) {
+		log.Println("Installing", what)
+		err = getter.GetFile(path, url)
+	}
+
+	if os.IsExist(err) {
+		log.Println("Located executable:", path)
+	}
+
+	Errc(err)
+	
+	return path
+}
+
+func PfxKill(dirs *Dirs) {
+	log.Println("Killing wineprefix")
+	Exec(dirs, "wineserver", "-k")
+}

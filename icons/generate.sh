dd082cb7f616d4fb2dab8b5e2f33a5f76882c0a1
lunarlattice <lunar.lattice0@gmail.com>
2023-02-08 11:46:05 -0800

    Update icons for flatpak

--- icons/generate.sh
+++ icons/generate.sh
@@ -8,6 +8,6 @@ for size in 16 32 48 64 128; do
 
 	mkdir -p $out
 	
-	convert roblox-player.png -resize $size $out/player.png
-	convert roblox-studio.png -resize $size $out/studio.png
+	convert roblox-player.png -resize $size $out/com.github.vinegar.roblox.player.png
+	convert roblox-studio.png -resize $size $out/com.github.vinegar.roblox.studio.png
 done

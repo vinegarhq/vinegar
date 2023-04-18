e129cd3e9ec346cfb333918142087e656dc87218
sewn <sewn@disroot.org>
2023-04-18 19:36:25 +0300

    config: refactor, template'd configuration somewhere else

--- config.go
+++ config.go
@@ -10,6 +10,7 @@ import (
 )
 
 type Configuration struct {
+	// Tagging required for the pretty print to make sense
 	RCO      bool                   `toml:"rco"`
 	AutoKill bool                   `toml:"autokill"`
 	Dxvk     bool                   `toml:"dxvk"`
@@ -23,26 +24,62 @@ type Configuration struct {
 	FFlags   map[string]interface{} `toml:"fflags"`
 }
 
+// Global Configuration for all of Vinegar's functions to be able to access.
+// The configuration will ALWAYS be loaded upon vinegar's launch.
 var (
 	ConfigFilePath = filepath.Join(Dirs.Config, "config.toml")
-	Config         = loadConfig()
+	Config         = LoadConfigFile()
 )
 
-func defConfig() Configuration {
+func LoadConfigFile() Configuration {
+	config := DefaultConfig()
+
+	if _, err := os.Stat(ConfigFilePath); errors.Is(err, os.ErrNotExist) {
+		log.Println("Using default configuration")
+
+		return config
+	}
+
+	if _, err := toml.DecodeFile(ConfigFilePath, &config); err != nil {
+		log.Fatal(err)
+	}
+
+	if config.Prime {
+		config.Env["DRI_PRIME"] = "1" // nouveau
+		config.Env["__NV_PRIME_RENDER_OFFLOAD"] = "1"
+		config.Env["__VK_LAYER_NV_optimus"] = "NVIDIA_only"
+		config.Env["__GLX_VENDOR_LIBRARY_NAME"] = "nvidia"
+	}
+
+	if config.WineRoot != "" {
+		config.Env["PATH"] = filepath.Join(config.WineRoot, "bin") + ":" + os.Getenv("PATH")
+	}
+
+	if config.Dxvk {
+		// Tells wine to use the DXVK DLLs
+		config.Env["WINEDLLOVERRIDES"] += "d3d10core=n;d3d11=n;d3d9=n;dxgi=n"
+	}
+
+	for name, value := range config.Env {
+		os.Setenv(name, value)
+	}
+
+	return config
+}
+
+// Default or 'sane' configuration of Vinegar.
+func DefaultConfig() Configuration {
 	return Configuration{
 		RCO:      true,
 		AutoKill: true,
 		Dxvk:     true,
 		Log:      true,
 		Prime:    false,
-		Launcher: "",
 		Renderer: "D3D11",
 		Version:  "win10",
-		WineRoot: "",
 		Env: map[string]string{
-			"WINEPREFIX": Dirs.Pfx,
-			"WINEARCH":   "win64",
-			// "WINEDEBUG":        "fixme-all,-wininet,-ntlm,-winediag,-kerberos",
+			"WINEPREFIX":       Dirs.Pfx,
+			"WINEARCH":         "win64",
 			"WINEDEBUG":        "-all",
 			"WINEESYNC":        "1",
 			"WINEDLLOVERRIDES": "dxdiagn=d;winemenubuilder.exe=d;",
@@ -54,64 +91,7 @@ func defConfig() Configuration {
 			"MESA_GL_VERSION_OVERRIDE":    "4.4",
 			"__GL_THREADED_OPTIMIZATIONS": "1",
 		},
-		FFlags: make(map[string]interface{}),
 	}
-}
-
-func writeConfigTemplate() {
-	if err := Mkdirs(Dirs.Config); err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Creating configuration template")
-
-	file, err := os.Create(ConfigFilePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	template := "# See how to configure Vinegar on the documentation website:\n" +
-		"# https://vinegarhq.github.io/Configuration\n\n"
-
-	if _, err = file.WriteString(template); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func (c *Configuration) Post() {
-	if c.Prime {
-		c.Env["DRI_PRIME"] = "1"
-		c.Env["__NV_PRIME_RENDER_OFFLOAD"] = "1"
-		c.Env["__VK_LAYER_NV_optimus"] = "NVIDIA_only"
-		c.Env["__GLX_VENDOR_LIBRARY_NAME"] = "nvidia"
-	}
-
-	if c.WineRoot != "" {
-		log.Println("Using Wine Root")
-		os.Setenv("PATH", filepath.Join(c.WineRoot, "bin")+":"+os.Getenv("PATH"))
-	}
-
-	for name, value := range c.Env {
-		os.Setenv(name, value)
-	}
-}
-
-func loadConfig() Configuration {
-	config := defConfig()
-
-	if _, err := os.Stat(ConfigFilePath); errors.Is(err, os.ErrNotExist) {
-		writeConfigTemplate()
-	} else if err != nil {
-		log.Fatal(err)
-	}
-
-	if _, err := toml.DecodeFile(ConfigFilePath, &config); err != nil {
-		log.Fatal("could not parse configuration file:", err)
-	}
-
-	config.Post()
-
-	return config
 }
 
 func (c *Configuration) Print() {

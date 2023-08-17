7e18d160247fc5cf5648844d188dab3e3f905c02
sewn <sewn@disroot.org>
2023-08-17 22:28:21 +0300

    aubun: minor refactor

--- aubun.go
+++ aubun.go
@@ -14,7 +14,7 @@ import (
 	"github.com/vinegarhq/aubun/wine/dxvk"
 )
 
-func Setup(ver bootstrapper.Version, dir string) {
+func SetupBinary(ver bootstrapper.Version, dir string) {
 	if err := dirs.Mkdir(dir); err != nil {
 		log.Fatal(err)
 	}
@@ -49,27 +49,27 @@ func Setup(ver bootstrapper.Version, dir string) {
 	}
 }
 
-func Binary(pfx *wine.Prefix, bin bootstrapper.BinaryType, cfg config.Application, allDxvk bool, args ...string) {
-	ver, err := bootstrapper.LatestVersion(bin, cfg.Channel)
-	if err != nil {
-		log.Fatal(err)
-	}
-	verDir := filepath.Join(dirs.Versions, ver.GUID)
+func Binary(bin bootstrapper.BinaryType, cfg *config.Config, pfx *wine.Prefix, args ...string) {
+	var appCfg config.Application
+	var exe string
 
-	_, err = os.Stat(filepath.Join(verDir, "AppSettings.xml"))
-	if err != nil {
-		log.Printf("Updating %s", bin.String())
-		Setup(ver, verDir)
-	} else {
-		log.Printf("%s is up to date (%s)", bin.String(), ver.GUID)
-	}
-
-	dxvkInstalled, err := state.DxvkInstalled()
-	if err != nil {
-		log.Fatal(err)
+	switch bin {
+	case bootstrapper.Player:
+		appCfg = cfg.Player
+		exe = "RobloxPlayerBeta.exe"
+	case bootstrapper.Studio:
+		appCfg = cfg.Player
+		exe = "RobloxStudioBeta.exe"
+	default:
+		log.Fatal("invalid binary type given")
 	}
 
-	if cfg.Dxvk {
+	if appCfg.Dxvk {
+		dxvkInstalled, err := state.DxvkInstalled()
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		if !dxvkInstalled {
 			if err := dxvk.Fetch(dirs.Cache); err != nil {
 				log.Fatal(err)
@@ -85,7 +85,7 @@ func Binary(pfx *wine.Prefix, bin bootstrapper.BinaryType, cfg config.Applicatio
 		}
 
 		dxvk.Setenv()
-	} else if allDxvk {
+	} else if !cfg.Player.Dxvk && !cfg.Studio.Dxvk {
 		if err := dxvk.Remove(pfx); err != nil {
 			log.Fatal(err)
 		}
@@ -95,20 +95,36 @@ func Binary(pfx *wine.Prefix, bin bootstrapper.BinaryType, cfg config.Applicatio
 		}
 	}
 
-	var exe string
-
-	switch bin {
-	case bootstrapper.Player:
-		exe = "RobloxPlayerBeta.exe"
-	case bootstrapper.Studio:
-		exe = "RobloxStudioBeta.exe"
+	channel := appCfg.Channel
+	if strings.HasPrefix(strings.Join(args, " "), "roblox-player:1") {
+		args, channel = bootstrapper.ParsePlayerURI(args[0])
 	}
 
-	log.Printf("Launching %s", bin)
+	if channel != appCfg.Channel {
+		log.Printf("WARNING: Launch URI has a different channel: %s, forcing user-specified channel", channel)
+		channel = appCfg.Channel
+	}
+
+	ver, err := bootstrapper.LatestVersion(bin, channel)
+	if err != nil {
+		log.Fatal(err)
+	}
+	verDir := filepath.Join(dirs.Versions, ver.GUID)
+
+	_, err = os.Stat(filepath.Join(verDir, "AppSettings.xml"))
+	if err != nil {
+		log.Printf("Updating/Installing %s", bin.String())
+		SetupBinary(ver, verDir)
+	} else {
+		log.Printf("%s is up to date (%s)", bin.String(), ver.GUID)
+	}
+
+	log.Printf("Launching %s", bin.String())
+
 	args = append([]string{filepath.Join(verDir, exe)}, args...)
 
 	if err := pfx.Exec(args...); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
 
@@ -118,8 +134,6 @@ func main() {
 	cfg := config.Load()
 	cfg.Setenv()
 
-	allEnabledDxvk := cfg.Player.Dxvk && cfg.Studio.Dxvk
-
 	pfx := wine.New(dirs.Prefix, "")
 	if err := pfx.Setup(); err != nil {
 		log.Fatal(err)
@@ -127,20 +141,9 @@ func main() {
 
 	switch os.Args[1] {
 	case "player":
-		args := os.Args[2:]
-		channel := cfg.Player.Channel
-
-		if strings.HasPrefix(strings.Join(args, " "), "roblox-player:1") {
-			args, channel = bootstrapper.ParsePlayerURI(args[0])
-		}
-
-		if channel != cfg.Player.Channel && channel != "" {
-			log.Printf("WARNING: Launch URI has a different channel: %s, forcing user-specified channel", channel)
-		}
-
-		Binary(&pfx, bootstrapper.Player, cfg.Player, allEnabledDxvk, args...)
+		Binary(bootstrapper.Player, &cfg, &pfx, os.Args[2:]...)
 	case "studio":
-		Binary(&pfx, bootstrapper.Studio, cfg.Studio, allEnabledDxvk, os.Args[2:]...)
+		Binary(bootstrapper.Studio, &cfg, &pfx, os.Args[2:]...)
 	case "exec":
 		if err := pfx.Exec(os.Args[2:]...); err != nil {
 			log.Fatal(err)

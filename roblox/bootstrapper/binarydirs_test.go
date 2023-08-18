64e2c9e07a42c2587af462cd2a256154537de3cf
sewn <sewn@disroot.org>
2023-08-18 21:24:54 +0300

    roblox/bootstrapper: fix tests

--- roblox/bootstrapper/binarydirs_test.go
+++ roblox/bootstrapper/binarydirs_test.go
@@ -3,16 +3,17 @@ package bootstrapper
 import (
 	"reflect"
 	"testing"
+	"github.com/vinegarhq/aubun/roblox"
 )
 
 func TestProperBinaryDirectories(t *testing.T) {
-	if !reflect.DeepEqual(Player.Directories(), PlayerDirectories) {
+	if !reflect.DeepEqual(Directories(roblox.Player), PlayerDirectories) {
 		t.Fatal("Player's directories is not the player's stored package directories")
 	}
-	if !reflect.DeepEqual(Studio.Directories(), StudioDirectories) {
+	if !reflect.DeepEqual(Directories(roblox.Studio), StudioDirectories) {
 		t.Fatal("Player's directories is not the player's stored package directories")
 	}
-	if BinaryType(-1).Directories() != nil {
+	if Directories(roblox.BinaryType(-1)) != nil {
 		t.FailNow()
 	}
 }

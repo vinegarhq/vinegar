13516de8e7f19801e7c7791570734d4b8f951def
sewn <sewn@disroot.org>
2023-09-24 09:50:20 +0300

    wine: proper registry types

--- wine/registry.go
+++ wine/registry.go
@@ -8,18 +8,18 @@ type RegistryType string
 
 const (
 	REG_SZ        RegistryType = "REG_SZ"
-	REG_MULTI_SZ               = "REG_MULTI_SZ"
-	REG_EXPAND_SZ              = "REG_EXPAND_SZ"
-	REG_DWORD                  = "REG_DWORD"
-	REG_QWORD                  = "REG_QWORD"
-	REG_BINARY                 = "REG_BINARY"
-	REG_NONE                   = "REG_NONE"
+	REG_MULTI_SZ  RegistryType = "REG_MULTI_SZ"
+	REG_EXPAND_SZ RegistryType = "REG_EXPAND_SZ"
+	REG_DWORD     RegistryType = "REG_DWORD"
+	REG_QWORD     RegistryType = "REG_QWORD"
+	REG_BINARY    RegistryType = "REG_BINARY"
+	REG_NONE      RegistryType = "REG_NONE"
 )
 
-func (p *Prefix) RegistryAdd(key, value string, kind, data string) error {
+func (p *Prefix) RegistryAdd(key, value string, rtype RegistryType, data string) error {
 	if key == "" {
 		return errors.New("no registry key given")
 	}
 
-	return p.Wine("reg", "add", key, "/v", value, "/t", kind, "/d", data, "/f").Run()
+	return p.Wine("reg", "add", key, "/v", value, "/t", string(rtype), "/d", data, "/f").Run()
 }
--- wine/tricks.go
+++ wine/tricks.go
@@ -7,5 +7,5 @@ import (
 func (p *Prefix) DisableCrashDialogs() error {
 	log.Println("Disabling Crash dialogs")
 
-	return p.RegistryAdd("HKEY_CURRENT_USER\\Software\\Wine\\WineDbg", "ShowCrashDialog", REG_DWORD, "")
+	return p.RegistryAdd("HKEY_CURRENT_USER\\Software\\Wine\\WineDbg", "ShowCrashDialog", REG_SZ, "")
 }

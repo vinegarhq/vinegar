4002d61bca0c8b6ec6627e7896140df8c64eadf5
sewn <sewn@disroot.org>
2023-09-24 09:55:35 +0300

    util: remove unused UserDataDir() function

--- util/paths.go
+++ util/paths.go
@@ -2,21 +2,7 @@ package util
 
 import (
 	"os"
-	"path/filepath"
 )
-
-func UserDataDir() (string, error) {
-	if dir := os.Getenv("XDG_DATA_HOME"); dir != "" {
-		return dir, nil
-	}
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
-
-	return filepath.Join(home, ".local", "share"), nil
-}
 
 func WalkDirExcluded(dir string, included []string, onExcluded func(string) error) error {
 	files, err := os.ReadDir(dir)

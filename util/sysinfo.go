a2422f56259185410ecd3abe41abe69e866cdfe1
Thelolguy1 <lunar.lattice0@gmail.com>
2023-10-16 14:00:31 -0700

    sysinfo.go: Initial structure created

---
+++ util/sysinfo.go
@@ -0,0 +1,30 @@
+/*
+This file is for detecting the user's system for helpdesk purposes.
+This is only a prototype.
+
+We will collect CPU flags and check for AVX, distro, kernel, flatpak, and config.
+More features may be added at a later time...
+
+ - lunarlattice 10/16/23
+*/
+
+package util
+
+import (
+	"github.com/vinegarhq/vinegar/internal/config"
+)
+
+type SysInfo struct {
+	CPUFlags	string
+	AVXAvailable    bool
+	Distro		string
+	Kernel		string
+	InFlatpak	bool
+	Config		config.Config
+}
+/*
+func GenerateDebug() (SysInfo){
+	//TODO, returns struct Sysinfo.
+
+}
+*/

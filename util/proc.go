f5dfc68093fdfebc28469288ca0f57ff7abbad7f
teknosquet <teknosquet@danwin1210.de>
2023-10-18 23:58:09 -0500

    util/proc.go: add *bsd support
    Signed-off-by: teknosquet <teknosquet@danwin1210.de>

--- util/proc.go
+++ util/proc.go
@@ -1,21 +1,34 @@
-//go:build linux
-
 package util
 
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 )
 
 func CommFound(query string) bool {
-	comms, _ := filepath.Glob("/proc/*/comm")
+	var comms, _ []string
+	switch runtime.GOOS {
+	case "linux":
+		comms, _ = filepath.Glob("/proc/*/comm")
+	default:
+		// FreeBSD, NetBSD, etc..
+		// Enabled procfs is required.
+		comms, _ = filepath.Glob("/proc/*/cmdline")
+	}
 
 	for _, comm := range comms {
 		c, err := os.ReadFile(comm)
-		// The 'comm' file contains a new line, we remove it, as it will mess up
-		// the query. hence 'minus'ing the length by 1 removes the newline.
-		if err == nil && strings.Contains(string(c)[:len(c)-1], query) {
+		// On Linux, the 'comm' file contains a newline, we remove it, as it
+		// will mess up the query. Hence 'minus'ing the length by 1 removes
+		// the newline.
+		if err == nil && strings.Contains(string(c)[:len(c)-1], query) &&
+		runtime.GOOS == "linux" {
+			return true
+		// On FreeBSD and NetBSD, this issue doesn't exist. Subtracting length
+		// by one doesn't work as 'cmdline' doesn't end in a newline character.
+		} else if err == nil && strings.Contains(string(c), query) {
 			return true
 		}
 	}

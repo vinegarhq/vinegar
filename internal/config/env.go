e0f46e1d96500ac32147d4f9a19fb79f7860f912
Jrelvas <55360900+Noted-Jrelvas@users.noreply.github.com>
2023-10-24 21:16:47 +0100

    env: use self directly in set

--- internal/config/env.go
+++ internal/config/env.go
@@ -9,12 +9,10 @@ type Environment map[string]string
 // Set will only set the given environment key and value if it isn't already set
 // within the Environment.
 func (e *Environment) Set(key, value string) {
-	env := *e
-
-	if _, ok := env[key]; ok {
+	if _, ok := (*e)[key]; ok {
 		return
 	}
-	env[key] = value
+	(*e)[key] = value
 }
 
 func (e *Environment) Setenv() {

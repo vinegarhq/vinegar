bb13dbce7268c3f6744ca722e044ee406899b7b6
Jrelvas <55360900+Noted-Jrelvas@users.noreply.github.com>
2023-10-24 17:20:17 +0100

    Remove log for env set
    Co-authored-by: sewn <sewn@disroot.org>
    Signed-off-by: Jrelvas <55360900+Noted-Jrelvas@users.noreply.github.com>

--- internal/config/env.go
+++ internal/config/env.go
@@ -11,7 +11,6 @@ type Environment map[string]string
 // within the Environment.
 func (e *Environment) Set(k string, v string) {
 	if _, ok := e[k]; ok {
-		log.Printf("Warning: env var %s is already defined. Will not redefine it.", k)
 		return
 	}
 	e[k] = v

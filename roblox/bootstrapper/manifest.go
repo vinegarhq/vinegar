078156c615fb1e28e84a45e08a23601ce9a6e31b
sewn <sewn@disroot.org>
2023-11-03 10:08:02 +0300

    roblox/bootstrapper: merge package and manifest, add unit test

--- roblox/bootstrapper/manifest.go
+++ roblox/bootstrapper/manifest.go
@@ -1,12 +1,15 @@
 package bootstrapper
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/vinegarhq/vinegar/roblox/version"
 	"github.com/vinegarhq/vinegar/util"
+	"golang.org/x/sync/errgroup"
 )
 
 type Manifest struct {
@@ -14,6 +17,19 @@ type Manifest struct {
 	DeployURL string
 	Packages
 }
+
+var (
+	ErrInvalidManifest          = errors.New("invalid package manifest given")
+	ErrUnhandledManifestVersion = errors.New("unhandled package manifest version")
+)
+
+type Package struct {
+	Name     string
+	Checksum string
+	Size     int64
+}
+
+type Packages []Package
 
 func channelPath(channel string) string {
 	// Ensure that the channel is lowercased, since internally in
@@ -52,4 +68,50 @@ func FetchManifest(ver *version.Version) (Manifest, error) {
 		DeployURL: durl,
 		Packages:  pkgs,
 	}, nil
+}
+
+func ParsePackages(manifest []string) (Packages, error) {
+	pkgs := make(Packages, 0)
+
+	if (len(manifest)-1)%4 != 0 {
+		return pkgs, ErrInvalidManifest
+	}
+
+	if manifest[0] != "v0" {
+		return pkgs, fmt.Errorf("%w: %s", ErrUnhandledManifestVersion, manifest[0])
+	}
+
+	for i := 1; i <= len(manifest)-4; i += 4 {
+		if manifest[i] == "RobloxPlayerLauncher.exe" ||
+			manifest[i] == "WebView2RuntimeInstaller.zip" {
+			continue
+		}
+
+		size, err := strconv.ParseInt(manifest[i+3], 10, 64)
+		if err != nil {
+			return pkgs, err
+		}
+
+		pkgs = append(pkgs, Package{
+			Name:     manifest[i],
+			Checksum: manifest[i+1],
+			Size:     size,
+		})
+	}
+
+	return pkgs, nil
+}
+
+func (pkgs *Packages) Perform(fn func(Package) error) error {
+	var eg errgroup.Group
+
+	for _, pkg := range *pkgs {
+		pkg := pkg
+
+		eg.Go(func() error {
+			return fn(pkg)
+		})
+	}
+
+	return eg.Wait()
 }

82ee7681e0a9b3564c503a9aabb7e77ec8f99bdf
Jrelvas <55360900+Noted-Jrelvas@users.noreply.github.com>
2023-11-24 22:28:18 +0000

    Merged gamemode.go and bus.go together

--- internal/bus/bus.go
+++ internal/bus/bus.go
@@ -1,6 +1,8 @@
 package bus
 
 import (
+	"errors"
+
 	"github.com/godbus/dbus/v5"
 )
 
@@ -19,4 +21,23 @@ func NewSession() *SessionBus {
 		conn:   conn,
 		portal: conn.Object("org.freedesktop.portal.Desktop", "/org/freedesktop/portal/desktop"),
 	}
+}
+
+func (s *SessionBus) GamemodeRegister(pid int) (bool, error) {
+	if s.conn == nil {
+		return false, nil
+	}
+
+	call := s.portal.Call("org.freedesktop.portal.GameMode.RegisterGame", 0, int32(pid))
+	if call.Err != nil {
+		//Transparently handle missing portal
+		if !errors.Is(call.Err, dbus.ErrMsgNoObject) {
+			return false, nil
+		}
+		return false, call.Err
+	}
+
+	response := call.Body[0].(int32)
+
+	return response > 0, nil
 }

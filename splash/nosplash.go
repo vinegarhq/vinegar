f1011656c805ab364691935a660f0fabbbbcd984
sewn <sewn@disroot.org>
2023-12-17 12:45:12 +0300

    splash: fix nosplash

--- splash/nosplash.go
+++ splash/nosplash.go
@@ -32,8 +32,8 @@ func (ui *Splash) IsClosed() bool {
 	return true
 }
 
-func (ui *Splash) Dialog(title, msg string) {
-	log.Printf("splash: dialog: %s %s", title, msg)
+func (ui *Splash) Dialog(title, msg string, yesNo bool) {
+	log.Printf("splash: dialog(%d): %s %s", yesNo, title, msg)
 }
 
 func New(cfg *Config) *Splash {

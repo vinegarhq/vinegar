51d2600137ede98d05fb5b9e88f9a5c443d2e214
sewn <sewn@disroot.org>
2023-12-21 22:51:14 +0300

    splash: drop nosplash support

--- splash/dialog.go
+++ splash/dialog.go
@@ -1,5 +1,3 @@
-//go:build !nogui && !nosplash
-
 package splash
 
 import (
--- splash/splash.go
+++ splash/splash.go
@@ -1,5 +1,3 @@
-//go:build !nogui && !nosplash
-
 // Package splash contains routines to run Vinegar's splash window.
 package splash
 
@@ -30,6 +28,18 @@ import (
 var vinegarLogo []byte
 
 var ErrClosed = errors.New("window closed")
+
+type Config struct {
+	Enabled     bool   `toml:"enabled"`     // Determines if splash is shown or not
+	LogoPath    string `toml:"logo_path"`   // Logo file path used to load and render the logo
+	Style       string `toml:"style"`       // Style to use for the splash layout
+	BgColor     uint32 `toml:"background"`  // Foreground color
+	FgColor     uint32 `toml:"foreground"`  // Background color
+	CancelColor uint32 `toml:"cancel,red"`  // Background color for the Cancel button
+	AccentColor uint32 `toml:"accent"`      // Color for progress bar's track and ShowLog button
+	TrackColor  uint32 `toml:"track,gray1"` // Color for the progress bar's background
+	InfoColor   uint32 `toml:"info,gray2"`  // Foreground color for the text containing binary information
+}
 
 type Splash struct {
 	*app.Window
@@ -195,3 +205,4 @@ func (ui *Splash) Run() error {
 
 	return nil
 }
+package splash

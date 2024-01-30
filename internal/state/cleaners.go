c49245f378730e320f46001cf0b0c9088bf50282
sewn <sewn@disroot.org>
2024-01-30 21:36:57 +0300

    internal/state: move used util func internal

--- internal/state/cleaners.go
+++ internal/state/cleaners.go
@@ -3,9 +3,10 @@ package state
 import (
 	"log"
 	"os"
+	"path/filepath"
+	"slices"
 
 	"github.com/vinegarhq/vinegar/internal/dirs"
-	"github.com/vinegarhq/vinegar/util"
 )
 
 // CleanPackages removes all cached package downloads in dirs.Downloads
@@ -13,7 +14,7 @@ import (
 func (s *State) CleanPackages() error {
 	log.Println("Checking for unused cached package files")
 
-	return util.WalkDirExcluded(dirs.Downloads, s.Packages(), func(path string) error {
+	return walkDirExcluded(dirs.Downloads, s.Packages(), func(path string) error {
 		log.Printf("Removing unused package %s", path)
 		return os.Remove(path)
 	})
@@ -24,8 +25,29 @@ func (s *State) CleanPackages() error {
 func (s *State) CleanVersions() error {
 	log.Println("Checking for unused version directories")
 
-	return util.WalkDirExcluded(dirs.Versions, s.Versions(), func(path string) error {
+	return walkDirExcluded(dirs.Versions, s.Versions(), func(path string) error {
 		log.Printf("Removing unused version directory %s", path)
 		return os.RemoveAll(path)
 	})
+}
+
+// walkDirExcluded will walk the file tree located at dir, calling
+// onExcluded for every file or directory that does not have a name in included.
+func walkDirExcluded(dir string, included []string, onExcluded func(string) error) error {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if slices.Contains(included, file.Name()) {
+			continue
+		}
+
+		if err := onExcluded(filepath.Join(dir, file.Name())); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }

c733382139871d0d1c1bbcf9a172ef640b8f64a3
sewn <sewn@disroot.org>
2024-02-02 08:54:55 +0300

    internal/dirs: multi-prefix propagation

--- internal/dirs/dirs.go
+++ internal/dirs/dirs.go
@@ -8,28 +8,18 @@ import (
 )
 
 var (
-	Cache      = filepath.Join(xdg.CacheHome, "vinegar")
-	Config     = filepath.Join(xdg.ConfigHome, "vinegar")
-	Data       = filepath.Join(xdg.DataHome, "vinegar")
-	Overlay    = filepath.Join(Config, "overlay")
-	Downloads  = filepath.Join(Cache, "downloads")
-	Logs       = filepath.Join(Cache, "logs")
-	Prefix     string
-	PrefixData string
-	Versions   string
-)
+	Cache     = filepath.Join(xdg.CacheHome, "vinegar")
+	Config    = filepath.Join(xdg.ConfigHome, "vinegar")
+	Data      = filepath.Join(xdg.DataHome, "vinegar")
+	Overlay   = filepath.Join(Config, "overlay")
+	Downloads = filepath.Join(Cache, "downloads")
+	Logs      = filepath.Join(Cache, "logs")
+	Prefixes  = filepath.Join(Data, "prefixes")
+	Versions  = filepath.Join(Data, "versions")
 
-func init() {
+	// Deprecated: Vinegar supports multiple wine prefixes
 	Prefix = filepath.Join(Data, "prefix")
-	envPrefix := os.Getenv("WINEPREFIX")
-
-	if filepath.IsAbs(envPrefix) {
-		Prefix = envPrefix
-	}
-
-	PrefixData = filepath.Join(Prefix, "vinegar")
-	Versions = filepath.Join(PrefixData, "versions")
-}
+)
 
 func Mkdirs(dirs ...string) error {
 	for _, dir := range dirs {

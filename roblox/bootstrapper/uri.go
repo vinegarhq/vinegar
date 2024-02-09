e7b6248f1b8f10a3ffea60c0e7279ddca331928f
sewn <sewn@disroot.org>
2024-02-09 11:29:40 +0300

    roblox/bootstrapper: minor doc change chapter 2

--- roblox/bootstrapper/uri.go
+++ roblox/bootstrapper/uri.go
@@ -7,6 +7,11 @@ import (
 // ProtocolURI is a representation of a Roblox web provided
 // URI which is sent to Player using MIME as a command line argument.
 //
+// The protocol URI is formatted in a hacky way so it is easy to parse
+// as a key:value seperated with the colon character.
+//
+// The order may matter, but it seems that the client parses it normal.
+//
 // Example:
 // 	roblox-player:1+channel:ganesh
 // 	roblox-player:1+channel:

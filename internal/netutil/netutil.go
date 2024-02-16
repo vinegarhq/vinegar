3ee0490426cdc9ca4825158b6ac84c31dcb380eb
sewn <sewn@disroot.org>
2024-02-16 20:13:07 +0300

    internal/netutil: introduce progress tracking for downloads

--- internal/netutil/netutil.go
+++ internal/netutil/netutil.go
@@ -10,9 +10,58 @@ import (
 	"os"
 )
 
+// DrawFunc is the callback type for drawing progress, it will
+// be ran in a goroutine.
+type DrawFunc func(float32)
+
+type progressCounter struct {
+	total   uint64
+	current uint64
+	draw    DrawFunc
+}
+
+func (pc *progressCounter) Write(p []byte) (int, error) {
+	n := len(p)
+	pc.current += uint64(n)
+	go pc.draw(float32(pc.current) / float32(pc.total))
+	return n, nil
+}
+
 // ErrBadStatus is the error returned by Download and Body
 // if the returned HTTP status code is not http.StatusOK.
 var ErrBadStatus = errors.New("bad status")
+
+// DownloadProgress downloads the named url to the named file, using
+// df as the callback for progress. No retry will be checked here.
+func DownloadProgress(url, file string, df DrawFunc) error {
+	out, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("%w: %s", ErrBadStatus, resp.Status)
+	}
+
+	pc := &progressCounter{
+		total: uint64(resp.ContentLength),
+		draw:  df,
+	}
+
+	_, err = io.Copy(out, io.TeeReader(resp.Body, pc))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Download downloads the named url to the named file. If an error
 // occurs when downloading the file. Download will retry 3 times before

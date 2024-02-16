33ae6f01cb28ac2cf991f6ed1f1f89c2a60aa90a
sewn <sewn@disroot.org>
2024-02-16 20:26:29 +0300

    i should make this a flag eventually, keep debugging it...

--- cmd/vinegar/binary.go
+++ cmd/vinegar/binary.go
@@ -344,7 +344,7 @@ func (b *Binary) Tail(name string) {
 	}
 
 	for line := range t.Lines {
-		// fmt.Fprintln(b.Prefix.Stderr, line.Text)
+		fmt.Fprintln(b.Prefix.Stderr, line.Text)
 
 		if b.Config.DiscordRPC {
 			if err := b.Activity.HandleRobloxLog(line.Text); err != nil {

3507d28d69b9e4a44097f76dae11853f737b9cdf
astraness <astraness@6ty9.me>
2024-02-18 18:49:34 +0700

    internal/dirs: clean up

--- internal/dirs/copy_test.go
+++ internal/dirs/copy_test.go
@@ -7,15 +7,14 @@ import (
 	"path/filepath"
 )
 
-func TestDefault(t *testing.T) {
-	overlay := t.TempDir()
-	dest := t.TempDir()
-
+func createTestFile(t *testing.T, overlay, dest string) (string, string) {
 	if err := Mkdirs(filepath.Join(overlay, "content"), filepath.Join(dest, "content")); err != nil {
 		t.Fatal(err)
 	}
 
 	srcPath := filepath.Join(overlay, "content", "test_content.txt")
+	destPath := filepath.Join(dest, "content", "test_content.txt")
+
 	file, err :=  os.OpenFile(srcPath, os.O_CREATE|os.O_WRONLY, 0o755)
 	if err != nil {
 		t.Fatal(err)
@@ -30,61 +29,34 @@ func TestDefault(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	err = OverlayDir(overlay, dest)
+	return srcPath, destPath
+}
 
-	if err != nil {
+func test(t *testing.T, overlay, dest, srcPath, destPath string) {
+	if err := OverlayDir(overlay, dest); err != nil {
 		t.Fatal(err)
 	}
 
-	destPath := filepath.Join(dest, "content", "test_content.txt")
 
 	if res, err := CompareLink(srcPath, destPath); err != nil && !res {
 		t.Fatal(err)
 	}
 }
 
+func TestDefault(t *testing.T) {
+	overlay := t.TempDir()
+	dest := t.TempDir()
+
+	srcPath, destPath := createTestFile(t, overlay, dest)
+	test(t, overlay, dest, srcPath, destPath)
+}
+
 func TestAlreadyExists(t *testing.T) {
 	overlay := t.TempDir()
 	dest := t.TempDir()
 
-	if err := Mkdirs(filepath.Join(overlay, "content"), filepath.Join(dest, "content")); err != nil {
-		t.Fatal(err)
-	}
+	srcPath, destPath := createTestFile(t, overlay, dest)
 
-	srcPath := filepath.Join(overlay, "content", "test_content.txt")
-	file, err :=  os.OpenFile(srcPath, os.O_CREATE|os.O_WRONLY, 0o755)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if _, err := file.WriteString("MEOWMEOW TESTING YAHH!!"); err != nil {
-		file.Close()
-		t.Fatal(err)
-	}
-
-	if err := file.Close(); err != nil {
-		t.Fatal(err)
-	}
-
-	err = OverlayDir(overlay, dest)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	destPath := filepath.Join(dest, "content", "test_content.txt")
-
-	if res, err := CompareLink(srcPath, destPath); err != nil && !res {
-		t.Fatal(err)
-	}
-
-	err = OverlayDir(overlay, dest)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if res, err := CompareLink(srcPath, destPath); err != nil && !res {
-		t.Fatal(err)
-	}
+	test(t, overlay, dest, srcPath, destPath)
+	test(t, overlay, dest, srcPath, destPath)
 }

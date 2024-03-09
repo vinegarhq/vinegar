be02a5ead738648d75abd45eeb22820b29731ac1
sewn <sewn@disroot.org>
2024-03-09 14:38:35 +0300

    splash: remove unused func

--- splash/rgba.go
+++ splash/rgba.go
@@ -11,8 +11,3 @@ func rgb(c uint32) color.NRGBA {
 func argb(c uint32) color.NRGBA {
 	return color.NRGBA{A: uint8(c >> 24), R: uint8(c >> 16), G: uint8(c >> 8), B: uint8(c)}
 }
-
-func mulAlpha(c color.NRGBA, alpha uint8) color.NRGBA {
-	c.A = uint8(uint32(c.A) * uint32(alpha) / 0xFF)
-	return c
-}

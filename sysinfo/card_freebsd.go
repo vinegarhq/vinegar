1a49ce2d193561a4933491c4f34cda074a6c92d2
bonspry <83516136+bonspry@users.noreply.github.com>
2025-01-20 04:46:59 +0100

    Add files for FreeBSD support.
    Signed-off-by: bonspry <83516136+bonspry@users.noreply.github.com>

---
+++ sysinfo/card_freebsd.go
@@ -0,0 +1,56 @@
+//go:build freebsd
+
+package sysinfo
+
+import (
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+const drmPath = "/linsys/class/drm"
+
+// Determines if a Card is 'embedded' or not, by checking
+// if one of these displays belong to the card.
+var embeddedDisplays = []string{"eDP", "LVDS"}
+
+func getCards() (cs []Card) {
+	drmCards, _ := filepath.Glob(path.Join(drmPath, "card[0-9]"))
+
+	for i, c := range drmCards {
+		dev, _ := filepath.EvalSymlinks(path.Join(c, "device"))
+		driver, _ := filepath.EvalSymlinks(path.Join(dev, "driver"))
+		driver = path.Base(driver)
+
+		cs = append(cs, Card{
+			Index:    i,
+			Path:     c,
+			Device:   dev,
+			Driver:   driver,
+			Embedded: embedded(c),
+		})
+	}
+
+	return
+}
+
+// Walks over the drm path, and checks if there are any displays
+// that are matched with the card path and contain any of embeddedDisplays
+func embedded(cardPath string) (embed bool) {
+	filepath.Walk(drmPath, func(p string, f os.FileInfo, err error) error {
+		if !strings.HasPrefix(p, cardPath) {
+			return nil
+		}
+
+		for _, hwd := range embeddedDisplays {
+			if strings.Contains(p, hwd) {
+				embed = true
+			}
+		}
+
+		return nil
+	})
+
+	return
+}

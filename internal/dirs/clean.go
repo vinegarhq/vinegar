b26d6dfe267f31e5822a38ce96dd2ac1e5f8e429
sewn <sewn@disroot.org>
2025-01-31 21:05:04 +0300

    internal/state: move dir helper to internal/dirs

---
+++ internal/dirs/clean.go
@@ -0,0 +1,28 @@
+package dirs
+
+import (
+	"os"
+	"slices"
+	"path/filepath"
+)
+
+// WalkForExcluded will walk the file tree located at dir, calling
+// onExcluded for every file or directory that does not have a name in the given included.
+func WalkForExcluded(dir string, included []string, onExcluded func(string) error) error {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if slices.Contains(included, file.Name()) {
+			continue
+		}
+
+		if err := onExcluded(filepath.Join(dir, file.Name())); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
--- internal/state/cleaners.go
+++ internal/state/cleaners.go
@@ -3,8 +3,6 @@ package state
 import (
 	"log/slog"
 	"os"
-	"path/filepath"
-	"slices"
 
 	"github.com/vinegarhq/vinegar/internal/dirs"
 )
@@ -12,7 +10,7 @@ import (
 // CleanPackages removes all cached package downloads in dirs.Downloads
 // that aren't held in the state's Binary packages.
 func (s *State) CleanPackages() error {
-	return walkDirExcluded(dirs.Downloads, s.Studio.Packages, func(path string) error {
+	return dirs.WalkForExcluded(dirs.Downloads, s.Studio.Packages, func(path string) error {
 		slog.Info("Cleaning up unused cached package", "path", path)
 		return os.Remove(path)
 	})
@@ -21,29 +19,8 @@ func (s *State) CleanPackages() error {
 // CleanPackages removes all Binary versions that aren't
 // held in the state's Binary packages.
 func (s *State) CleanVersions() error {
-	return walkDirExcluded(dirs.Versions, []string{s.Studio.Version}, func(path string) error {
+	return dirs.WalkForExcluded(dirs.Versions, []string{s.Studio.Version}, func(path string) error {
 		slog.Info("Cleaning up unused version directory", "path", path)
 		return os.RemoveAll(path)
 	})
-}
-
-// walkDirExcluded will walk the file tree located at dir, calling
-// onExcluded for every file or directory that does not have a name in included.
-func walkDirExcluded(dir string, included []string, onExcluded func(string) error) error {
-	files, err := os.ReadDir(dir)
-	if err != nil {
-		return err
-	}
-
-	for _, file := range files {
-		if slices.Contains(included, file.Name()) {
-			continue
-		}
-
-		if err := onExcluded(filepath.Join(dir, file.Name())); err != nil {
-			return err
-		}
-	}
-
-	return nil
 }

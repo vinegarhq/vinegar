263b43002a9fe972f7089e2c4f64e02ee1baf6e4
sewn <sewn@disroot.org>
2025-02-06 17:05:59 +0300

    internal/dirs: use filepath.Walk

--- internal/dirs/clean.go
+++ internal/dirs/clean.go
@@ -9,20 +9,15 @@ import (
 // WalkForExcluded will walk the file tree located at dir, calling
 // onExcluded for every file or directory that does not have a name in the given included.
 func WalkForExcluded(dir string, included []string, onExcluded func(string) error) error {
-	files, err := os.ReadDir(dir)
-	if err != nil {
-		return err
-	}
-
-	for _, file := range files {
-		if slices.Contains(included, file.Name()) {
-			continue
-		}
-
-		if err := onExcluded(filepath.Join(dir, file.Name())); err != nil {
+	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
 			return err
 		}
-	}
 
-	return nil
+		if slices.Contains(included, info.Name()) || info.IsDir() {
+			return nil
+		}
+
+		return onExcluded(path)
+	})
 }

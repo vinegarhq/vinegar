fe08b10192d6b6008368857e5e4e831f571d64bb
sewn <sewn@disroot.org>
2025-08-08 00:24:14 +0300

    feat: add ROBLOSECURITY fetcher

---
+++ cmd/vinegar/security.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"crypto/rc4"
+	"errors"
+	"strings"
+)
+
+var credPath = `HKEY_CURRENT_USER\Software\Wine\Credential Manager`
+
+func (a *app) getSecurity() (string, error) {
+	keys, err := a.pfx.RegistryQuery(credPath, ``)
+	if err != nil {
+		return "", err
+	}
+
+	var c *rc4.Cipher
+	for _, k := range keys {
+		if k.Key != credPath && !strings.Contains(k.Key, "RobloxStudioAuth") {
+			continue
+		}
+		// following subkeys usually only belong to the ROBLOSECURITY or root
+		// credential registry key
+		for _, sk := range k.Subkeys {
+			switch sk.Name {
+			case "EncryptionKey": // [Software\Wine\Credential Manager
+				c, err = rc4.NewCipher([]byte(sk.Value))
+				if err != nil {
+					return "", err
+				}
+			case "Password": // Software\Wine\Credential Manager\Generic: https://www.roblox.com:RobloxStudioAuth.
+				sec := make([]byte, len(sk.Value))
+				c.XORKeyStream(sec, []byte(sk.Value))
+				return string(sec), nil
+			}
+		}
+	}
+
+	return "", errors.New("failed to locate security")
+}

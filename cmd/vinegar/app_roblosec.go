5ad2753f0b04453f66896b6340f0e439fe8ff9fc
sewn <sewn@disroot.org>
2025-12-25 20:48:16 +0300

    chore: remove login support

--- cmd/vinegar/app_roblosec.go
+++ cmd/vinegar/app_roblosec.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"crypto/rand"
 	"crypto/rc4"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"log/slog"
 	"slices"
-	"time"
 )
 
 var (
@@ -64,58 +61,4 @@ func keyStream(c *rc4.Cipher, subKey []byte) string {
 	sec := make([]byte, len(subKey))
 	cpy.XORKeyStream(sec, subKey)
 	return string(sec)
-}
-
-func (a *app) getWineCredKey() ([]byte, error) {
-	k, err := a.pfx.RegistryQuery(credPath, `EncryptionKey`)
-	if err != nil {
-		return nil, fmt.Errorf("query: %w", err)
-	}
-	if k != nil {
-		return k[0].Subkeys[0].Value.([]byte), nil
-	}
-
-	// Generate one since Wine will only do so if an application uses the
-	// Credential Manager API, not the registry.
-	enc := make([]byte, 8)
-	_, _ = rand.Read(enc[:])
-	if err := a.pfx.RegistryAdd(credPath, "EncryptionKey", enc); err != nil {
-		return nil, fmt.Errorf("add: %w", err)
-	}
-	return enc, nil
-}
-
-func (a *app) addWineCred(key []byte, name string, blob []byte) error {
-	c, err := rc4.NewCipher(key)
-	if err != nil {
-		return fmt.Errorf("cipher: %w", err)
-	}
-	encr := make([]byte, len(blob))
-	c.XORKeyStream(encr, blob)
-
-	slog.Info("Writing Wine Credential", "name", name)
-
-	for _, sk := range []struct {
-		name  string
-		value any
-	}{
-		{"", name}, // (Default)
-		{"Flags", uint32(0)},
-		{"LastWritten", filetime(time.Now())},
-		{"Password", encr},
-		{"Persist", uint32(2)},
-		{"Type", uint32(1)},
-	} {
-		if err := a.pfx.RegistryAdd(credPath+`\Generic: `+name, sk.name, sk.value); err != nil {
-			return fmt.Errorf("add %s: %w", sk.name, err)
-		}
-	}
-	return nil
-}
-
-func filetime(t time.Time) []byte {
-	b := make([]byte, 8)
-	binary.LittleEndian.PutUint64(b, uint64(
-		t.UTC().UnixNano()/100+116444736000000000))
-	return b
 }
